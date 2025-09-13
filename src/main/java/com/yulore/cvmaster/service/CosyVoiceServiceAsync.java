c939177241ef4aa2dee2ea76a2557366f1f69837
ming.ma <ming.ma@yulore.com>
2025-09-13 20:28:00 +0800

    add commitZeroShotTask define for CosyVoiceServiceAsync

--- src/main/java/com/yulore/cvmaster/service/CosyVoiceServiceAsync.java
+++ src/main/java/com/yulore/cvmaster/service/CosyVoiceServiceAsync.java
@@ -1,6 +1,7 @@
 package com.yulore.cvmaster.service;
 
 import com.yulore.api.CosyVoiceService;
+import com.yulore.api.ZeroShotTask;
 import org.redisson.api.RFuture;
 import org.redisson.api.annotation.RRemoteAsync;
 
@@ -11,4 +12,5 @@ public interface CosyVoiceServiceAsync {
                                               final String promptWav,
                                               final String bucket,
                                               final String saveTo);
+    RFuture<Void> commitZeroShotTask(final ZeroShotTask task);
 }
